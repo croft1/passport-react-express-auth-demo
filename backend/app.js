bdd76c7753251da9571c35218853212ba084c2f9
mewc <mikecarter771@gmail.com>
2018-05-01 10:46:58 +1000

    hitting endpoint fully setup with axios, getting cors error atm

--- backend/app.js
+++ backend/app.js
@@ -3,31 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 var passport = require('passport');
 var Strategy = require('passport-github').Strategy;
 
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 
 passport.use(new Strategy({
         // clientID: process.env.CLIENT_ID,
@@ -39,7 +34,6 @@ passport.use(new Strategy({
     function(accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
     }));
-
 
 passport.serializeUser(function(user, cb) {
     cb(null, user);
@@ -54,11 +48,11 @@ passport.deserializeUser(function(obj, cb) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Define routes.
 app.get('/',
     function(req, res) {
         res.render('home', { user: req.user });
+
     });
 
 app.get('/login',
@@ -74,7 +68,6 @@ app.get('/login/github/return',
     function(req, res) {
         res.redirect('/');
     });
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+
+import {FlatButton} from 'material-ui';
+import MTP from 'material-ui/styles/MuiThemeProvider';
+import getmt from 'material-ui/styles/getMuiTheme';
+import dark from 'material-ui/styles/baseThemes/darkBaseTheme';
 import './App.css';
 
+const githubLoginUrl = "http://localhost:3000/login/github";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    login(event) {
+        console.log("Login clicked");
+        console.log(event);
+        axios.get(githubLoginUrl)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    loginXhr(e) {
+        e.preventDefault();
+        let xhr = new XMLHttpRequest();
+        xhr.open('get', 'http://localhost:3000/login/github');
+        xhr.send();
+    }
+
+    render() {
+        return (
+            <MTP muiTheme={getmt(dark)}>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Passport React Express Auth Demo</h1>
+                    </header>
+                    <p className="App-intro">
+                        <FlatButton
+                            onClick={this.login.bind(this)}
+                            label={"Login with github"}
+                            primary={true}/>
+                    </p>
+                    <a href="https://mewc.info">Creator</a><br/>
+                    <a href="https://github.com/croft1/passport-react-express-auth-demo">Repo</a>
+                </div>
+            </MTP>
+        );
+    }
 }
 
 export default App;
